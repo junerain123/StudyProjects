e1e615c4d4a6314fa13e0d4e45c4e1bab5f47b03
junerain123 <wang1780089183@163.com>
2021-02-25 10:40:38 +0800

    1

---
+++ Net/Blazor.WebAssembly/Components/Home.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.WebAssembly.Components
+{
+    public partial class Home
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
+
+        //color是上级组件传递来的，依据string类型
+        [CascadingParameter]
+        public string Color { get; set; }
+
+        //如果父组件中传参没有指定参数名，则ChildContent的名称是固定的
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        //如果父组件中传参指定了参数名HeadContent
+        [Parameter]
+        public RenderFragment HeadContent { get; set; }
+    }
+}
---
+++ Net/Blazor.WebAssembly/Shared/404.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.WebAssembly.Shared
+{
+    public partial class _404
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public void NavigateToHome()
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
